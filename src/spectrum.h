49540b411f4353c0ef3a8e6ffeb36a81ab190a8d
nkshr <naokasa@outlook.jp>
2017-07-28 16:16:33 +0900

    cstring is included in spectrum.h

--- src/spectrum.h
+++ src/spectrum.h
@@ -4,6 +4,7 @@
 #include <utility>
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 #include "vector.h"
 #include "common.h"
--- src/spectrum_test.cpp
+++ src/spectrum_test.cpp
@@ -2,11 +2,8 @@
 
 #include "spectrum.h"
 
-#define NUM_SAMPLES 10
-#define MIN_LAMBDA 0
-#define MAX_LAMBDA 10
 int main(int argc, char ** argv) {
-	Spectrum<NUM_SAMPLES, MIN_LAMBDA, MAX_LAMBDA> a, b, c;
+	SpectrumX<NUM_SAMPLES, MIN_LAMBDA, MAX_LAMBDA> a, b, c;
 	a.load("");
 	return 0;
 }
