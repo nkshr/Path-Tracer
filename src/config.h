0dd480ee4ebdda0fb924bacf713c1a6167f9d54c
Kasahara Nao <naokasa@outlook.jp>
2017-08-17 23:46:52 +0900

    remove static and define pi.

--- src/config.h
+++ src/config.h
@@ -2,22 +2,24 @@
 #define CONFIG_H
 
 namespace config {
-	static constexpr int number_of_samples_per_spectrum = 41;
-	static constexpr double minimum_lambda = 380.0;
-	static constexpr double maximum_lambda = 780.0;
+	constexpr int number_of_samples_per_spectrum = 41;
+	constexpr double minimum_lambda = 380.0;
+	constexpr double maximum_lambda = 780.0;
 	//static constexpr int number_of_samples_per_pixel = 20;
 	//static constexpr int number_of_samples_per_intersection = 1;
 	//static constexpr int number_of_samples_per_point = 1;
 	//static constexpr int maximum_depth = 4;
-	static const char * absorption_coefficients_file = "../data/Pope_abosorp_coefs.csv";
-	static const char * scattering_coefficients_file = "../data";
-	static const char * gt1290_eq_file = "../data/gt1290.csv";
-	static const char * mako_r_eq_file = "../data/mako_r.csv";
-	static const char * mako_g_eq_file = "../data/mako_g.csv";
-	static const char * mako_b_eq_file = "../data/mako_b.csv";
-	static constexpr int buffer_size = 1024;
+	constexpr char* const absorption_coefficients_file = "../data/Pope_abosorp_coefs.csv";
+	constexpr char* const scattering_coefficients_file = "../data";
+	constexpr char* const gt1290_eq_file = "../data/gt1290.csv";
+	constexpr char* const mako_r_eq_file = "../data/mako_r.csv";
+	constexpr char* const mako_g_eq_file = "../data/mako_g.csv";
+	constexpr char* const mako_b_eq_file = "../data/mako_b.csv";
+	constexpr int buffer_size = 1024;
 	constexpr double lambda_step = (double)(maximum_lambda- minimum_lambda)/(double)(number_of_samples_per_spectrum - 1);
-	static constexpr double eps = 1e-4;
-	static constexpr double general_albedo = 0.56;
+	constexpr double eps = 1e-4;
+	constexpr double general_albedo = 0.56;
+	constexpr double pi = 3.14159265358979323846;
 };
+
 #endif
