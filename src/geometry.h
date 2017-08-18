c65cf7f8275a64958ce91ab69d46f09c380182d2
Kasahara Nao <naokasa@outlook.jp>
2017-08-18 14:34:46 +0900

    Add geo_ray_intersection(const Ray &ray, double &t) to Rectangle.

--- src/geometry.h
+++ src/geometry.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "ray.h"
+
+namespace geo {
+	class Geometry {
+		virtual bool get_ray_intersection(const Ray &ray, double &t) = 0;
+	};
+
+	class Rectangle : public Geometry {
+	private:
+		Vec m_p;
+		Vec m_n;
+		Vec m_u;
+		Vec m_t;
+
+		Vec m_x_dir;
+		Vec m_y_dir;
+
+		double m_w;
+		double m_h;
+
+	public:
+		Rectangle(Vec p, Vec n, Vec up, double w, double h);
+		virtual bool get_ray_intersection(const Ray &ray, double &t);
+	};
+};
