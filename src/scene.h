a0fd72ece02ebf7a0f5a4218827c717d7d84fa58
Kasahara Nao <naokasa@outlook.jp>
2017-08-19 00:54:42 +0900

    Move intersect function from Tracer to Scene.

--- src/scene.h
+++ src/scene.h
@@ -13,5 +13,30 @@ struct Scene {
 	std::vector<Object*> objects;
 	std::vector<Light*> lights;
 	Observer * observer;
+
+	ObjectIntersection intersect(const Ray &ray) {
+		ObjectIntersection isct = ObjectIntersection();
+		ObjectIntersection temp;
+		const size_t size = objects.size();
+
+		for (int i = 0; i<size; i++) {
+			temp = objects.at((unsigned)i)->get_intersection(ray);
+
+			if (temp.hit) {
+				if (isct.u == 0 || temp.u < isct.u) isct = temp;
+			}
+		}
+		return isct;
+	}
+
+	~Scene() {
+		for (int i = 0; i < objects.size(); ++i) {
+			delete objects[i];
+		}
+
+		for (int i = 0; i < lights.size(); ++i) {
+			delete lights[i];
+		}
+	}
 };
 #endif //SCENE_H
