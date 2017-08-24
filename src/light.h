4dfa2822cc7b706d66bf25eaf8b4b40f6e04c93e
Kasahara Nao <naokasa@outlook.jp>
2017-08-24 13:23:55 +0900

    Remove unused statements.

--- src/light.h
+++ src/light.h
@@ -5,55 +5,6 @@
 #include "ray.h"
 #include "geometry.h"
 
-//class Light {
-//protected:
-//public:
-//	virtual Vec get_shadow_ray_dir(const Vec &p) const = 0;
-//	virtual Spectrum get_spectral_radiance(const Ray& shadow_ray) const = 0;
-//	virtual Vec get_position() const = 0;
-//};
-//
-//class PointLight : public Light{
-//protected:
-//	Vec m_p;
-//	Spectrum m_srad;
-//
-//public:
-//	PointLight(Vec p, Spectrum spectral_radiance);
-//
-//	virtual Vec get_shadow_ray_dir(const Vec &p) const;
-//	virtual Spectrum get_spectral_radiance(const Ray& shadow_ray) const;
-//	virtual Vec get_position() const;
-//};
-//
-//class SpotLight : public Light{
-//private:
-//	Spectrum m_srad;
-//	double m_ang;
-//	double m_cos;
-//	Vec m_p;
-//	Vec m_d;
-//public:
-//	SpotLight(Vec p, Vec d, Spectrum srad, double deg);
-//
-//	virtual Vec get_shadow_ray_dir(const Vec &p) const;
-//	virtual Spectrum get_spectral_radiance(const Ray& shadow_ray) const;
-//	virtual Vec get_position() const;
-//};
-//
-//class Liser : public Light{
-//private:
-//	geo::Rectangle m_rect;
-//	Spectrum m_srad;
-//
-//public:
-//	Liser(Vec p, Vec target, Vec up, double w, double h, Spectrum srad);
-//
-//	virtual Vec get_shadow_ray_dir(const Vec &p) const;
-//	virtual Spectrum get_spectral_radiance(const Ray& shadow_ray) const;
-//	virtual Vec get_position() const;
-//};
-
 class PointLight : public Object {
 private:
 	Spectrum m_srad;
